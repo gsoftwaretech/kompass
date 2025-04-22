e5414f534370bdbdd474b2f7702e6a3fc6d24861
woz07 <muhsameer9@gmail.com>
2025-04-22 14:52:24 +0100

    add: stop button & more notif

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
     "fyne.io/fyne/v2/container"
     "fyne.io/fyne/v2/layout"
 	"github.com/go-toast/toast"
+    "github.com/go-vgo/robotgo"
+    "strconv"
 )
 
-var APPLICATION = app.NewWithID("kompass")
-var WINDOW = APPLICATION.NewWindow("Kompass - Writing Tool")
-var DelayInMs = 250 // Default delay in milliseconds per character
-var text = ""       // Text to be written
+var APPLICATION         = app.NewWithID("kompass")
+var WINDOW              = APPLICATION.NewWindow("Kompass - Writing Tool")
+var DisplayNotification = true // Flag to display notification
 
 func main() {
     WINDOW.Resize(fyne.NewSize(700, 350))
@@ -28,16 +29,58 @@ func main() {
     iDelayContainer := container.NewWithoutLayout(iDelayInMs)
     iDelayInMs.Resize(fyne.NewSize(200, iDelayInMs.MinSize().Height)) // Set custom width
 
+    StopWriting := false // Flag to stop writing
+    bStop   := widget.NewButton("Stop", func() {
+        StopWriting = true
+    })
+
     bSubmit := widget.NewButton("Write", func() {
+        StopWriting = false // Reset the stop flag
 		// Write text then send notificaiton
 
-		notification := toast.Notification{
-			AppID:   "Kompass",
-			Title:   "Kompass Notification",
-			Message: "Writing complete",
-		}
+        text       := iText.Text
+        delay, err := strconv.Atoi(iDelayInMs.Text)
+        if (err != nil) {
+            delay = 25 // Default delay of 25ms
+        }
 
-		notification.Push()
+        go func() {
+            // Set 2s delay
+            robotgo.MilliSleep(2000)
+            
+            for _, char := range text {
+                if StopWriting {
+                    if (DisplayNotification) {
+                        notification := toast.Notification{
+                            AppID:   "Kompass",
+                            Title:   "Kompass Notification",
+                            Message: "Writing stopped",
+                        }
+                        notification.Push()
+                    }   
+                    break // Stop writing if the button is tapped
+                }
+
+                if char == '\n' {
+                    // Simulate pressing the "Enter" key for newlines
+                    robotgo.KeyTap("enter")
+                } else {
+                    // Type the character
+                    robotgo.TypeStr(string(char))
+                }
+                robotgo.MilliSleep(delay)
+            }
+
+            if (DisplayNotification) {
+                notification := toast.Notification{
+                    AppID:   "Kompass",
+                    Title:   "Kompass Notification",
+                    Message: "Writing complete",
+                }
+        
+                notification.Push()
+            }
+        }()
 	})
 
     lCenter := container.NewMax(iText)
@@ -45,6 +88,7 @@ func main() {
         widget.NewLabel("Delay per character (ms):"),
         iDelayContainer,
         layout.NewSpacer(),
+        bStop,
         bSubmit,
     )
     content := container.NewBorder(
